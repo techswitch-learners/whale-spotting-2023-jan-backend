69836d0b3f52e9ef894ee20916ff9603c3172975
Tim Leach <timleach635@gmail.com>
2023-03-10 14:08:14 +0000

    Add endpoint for easy testing

---
+++ WhaleSpotting/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WhaleSpotting.Controllers;
+
+[ApiController]
+[Route("test")]
+public class TestController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult Test()
+    {
+        return Ok("The Whale Spotting API is running correctly!");
+    }
+}
