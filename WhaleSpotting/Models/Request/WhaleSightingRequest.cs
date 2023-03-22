5720244d28bd575c9bd7c605252c85e1dcd7453f
Keqi Chen <ckq009@gmail.com>
2023-03-22 12:23:19 +0000

    Add request model for whale sighting form

---
+++ WhaleSpotting/Models/Request/WhaleSightingRequest.cs
@@ -0,0 +1,12 @@
+namespace WhaleSpotting.Models.Request;
+
+public class WhaleSightingRequest
+{
+    public DateTime DateOfSighting { get; set; }
+    public float LocationLatitude { get; set; }
+    public float LocationLongitude { get; set; }
+    public string PhotoImageURL { get; set; }
+    public int NumberOfWhales { get; set; }
+    public string Description { get; set; }
+    public WhaleSpecies WhaleSpecies { get; set; }
+}
